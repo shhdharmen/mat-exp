002047c0f871b8a57af8af2c9f684706dd9da9f7
Dharmen Shah <shhdharmen@gmail.com>
2026-02-10 13:40:50 +0530

    refacotr: fix project names

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ngm-dev-components-docs');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mat-expressive-docs');
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('ngm-dev-components-docs');
+  protected readonly title = signal('mat-expressive-docs');
 }
